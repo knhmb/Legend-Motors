1d71a1920886de6d13512e5fdb0b07127f6dd3d3
knhmb <khaled@marvelconnect.com>
2023-10-25 13:02:54 +0800

    Updates

--- src/pages/ChangePassword.vue
+++ src/pages/ChangePassword.vue
@@ -1,42 +1,173 @@
 <template>
   <div class="change-password">
     <h3>Change Password</h3>
-    <el-form label-position="top">
-      <el-form-item label="Current Password">
+    <el-form
+      :model="ruleForm"
+      ref="ruleFormRef"
+      :rules="rules"
+      label-position="top"
+    >
+      <el-form-item label="Current Password" prop="currentPassword">
         <base-input
+          v-model="ruleForm.currentPassword"
           type="password"
           show-password
           placeholder="Current Password"
         ></base-input>
       </el-form-item>
-      <el-form-item label="Password">
+      <el-form-item label="Password" prop="newPassword">
         <base-input
+          v-model="ruleForm.newPassword"
           type="password"
           show-password
           placeholder="Password"
         ></base-input>
       </el-form-item>
-      <el-form-item label="Confirm Password">
+      <el-form-item label="Confirm Password" prop="confirmNewPassword">
         <base-input
+          v-model="ruleForm.confirmNewPassword"
           type="password"
           show-password
           placeholder="Confirm Password"
         ></base-input>
       </el-form-item>
       <el-form-item>
-        <base-button>Save</base-button>
+        <base-button @click="submit">Save</base-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
   
   <script>
+import { ElNotification } from "element-plus";
+import loading from "@/utils/loading";
+
 export default {
+  mixins: [loading],
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error(this.$t("auth.password-required")));
+      } else {
+        if (this.ruleForm.confirmNewPassword !== "") {
+          if (!this.$refs.ruleFormRef) return;
+          this.$refs.ruleFormRef.validateField(
+            "confirmNewPassword",
+            () => null
+          );
+        }
+        callback();
+      }
+    };
+    const validateConfirmPass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error(this.$t("auth.confirm-password-required")));
+      } else if (value !== this.ruleForm.newPassword) {
+        callback(new Error(this.$t("auth.password-dont-match")));
+      } else {
+        callback();
+      }
+    };
+
     return {
+      ruleForm: {
+        currentPassword: "",
+        newPassword: "",
+        confirmNewPassword: "",
+      },
+      rules: {
+        currentPassword: [
+          {
+            required: true,
+            message: "Current password is requried",
+            trigger: "blur",
+          },
+        ],
+        newPassword: [
+          { required: true, validator: validatePass, trigger: "blur" },
+        ],
+        confirmNewPassword: [
+          { required: true, validator: validateConfirmPass, trigger: "blur" },
+        ],
+      },
       fileList: [],
       email: "",
     };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.getters["auth/currentUser"];
+    },
+  },
+  methods: {
+    checkPassword(pass) {
+      const re = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+      return re.test(pass);
+    },
+    async changePassword(passwordData) {
+      this.openLoading();
+      await this.$store
+        .dispatch("auth/changePassword", passwordData)
+        .then(() => {
+          this.closeLoading();
+          this.$refs.ruleFormRef.resetField();
+          ElNotification({
+            title: "Success",
+            message: "Password changed",
+            type: "success",
+          });
+        })
+        .catch((err) => {
+          this.closeLoading();
+          ElNotification({
+            title: "Error",
+            message: this.$t(err.response.data.message),
+            type: "error",
+          });
+        });
+      // this.$store
+      //   .dispatch("auth/changePassword", passwordData)
+      //   .then(() => {
+      //     this.closeLoading();
+      //     this.$refs.ruleFormRef.resetField();
+      //     ElNotification({
+      //       title: "Success",
+      //       message: "Password Changed",
+      //       type: "success",
+      //     });
+      //   })
+      //   .catch((err) => {
+      //     this.closeLoading();
+      //     ElNotification({
+      //       title: "Error",
+      //       message: this.$t(err.response.data.message),
+      //       type: "error",
+      //     });
+      //   });
+    },
+    submit() {
+      this.$refs.ruleFormRef.validate(async (valid) => {
+        if (valid) {
+          const isPasswordValid = this.checkPassword(this.ruleForm.newPassword);
+          if (isPasswordValid) {
+            const data = {
+              currentPass: this.ruleForm.currentPassword,
+              password: this.ruleForm.newPassword,
+              password2: this.ruleForm.confirmNewPassword,
+              email: this.currentUser.email,
+            };
+            await this.changePassword(data);
+          } else {
+            ElNotification({
+              title: "Error",
+              message:
+                "Password requires at least one upper-case alphabet, lower-case alphabet, numberic.",
+              type: "error",
+            });
+          }
+        }
+      });
+    },
   },
 };
 </script>
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -14,7 +14,7 @@ export default {
   //   context.commit("LOGIN", response.data);
   // },
   async validateUser(context, payload) {
-    const response = await axios.get("/api/v1/authenticate", {
+    const response = await axios.get("api/v1/authenticate", {
       headers: {
         Authorization: `Bearer ${payload}`,
       },
@@ -23,7 +23,7 @@ export default {
   },
   async checkRefreshToken(context, payload) {
     const response = await axios.put(
-      "/api/v1/authenticate",
+      "api/v1/authenticate",
       {},
       {
         headers: {
@@ -42,7 +42,7 @@ export default {
   //   context.commit("LOGIN", response.data);
   // },
   async logout(context) {
-    await axios.delete("/api/v1/authenticate");
+    await axios.delete("api/v1/authenticate");
     context.commit("LOGOUT");
   },
   async getOTP(_, payload) {
@@ -61,5 +61,8 @@ export default {
   },
   async resetPassword(_, payload) {
     await axios.post("api/v1/accounts/reset-password", payload);
+  },
+  async changePassword(_, payload) {
+    await axios.post("api/v1/accounts/change-password", payload);
   },
 };
